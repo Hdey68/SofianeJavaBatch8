09dd1429c4acce53b2882fed604b39878b75eca6
Hdey68 <ouniche.sofiane@gmail.com>
2020-11-17 22:05:37 -0500

    commit from intellij

---
+++ class25/FinalParentClass.java
@@ -0,0 +1,14 @@
+package com.syntax.class25;
+
+
+    final public class FinalParentClass {
+        String name;
+
+        public FinalParentClass(String name) {
+            this.name = name;
+        }
+
+        public void printName() {
+            System.out.println("Name " + name);
+        }
+    }
